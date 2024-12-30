c3f4897b20dd2c6811ac8ff7ba67f639ed2a27eb
Satish <satish.venkatesh@circana.com>
2024-12-30 13:04:04 +0530

    Home: greeting Data Passing through Signals

---
+++ src/app/components/greeting/greeting.component.ts
@@ -0,0 +1,13 @@
+import {Component, input} from '@angular/core';
+
+@Component({
+  selector: 'app-greeting',
+  standalone: true,
+  imports: [],
+  templateUrl: './greeting.component.html',
+  styleUrl: './greeting.component.scss'
+})
+export class GreetingComponent {
+  message = input("Hello Hello");
+
+}
